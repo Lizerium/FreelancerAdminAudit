e0d17cd07f34c97bcdd2236b8d6869573a45a9d2
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-06 11:08:00 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-06

--- app/Program.cs
+++ app/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 сентября 2026 08:36:10
- * Version: 1.0.164
+ * Last Updated: 06 сентября 2026 11:08:00
+ * Version: 1.0.165
  */
 
 using System.Net.Sockets;
